caeb7982276a7e2d45f775cbb8bd25d7db3c6242
ik now <101111988+iknow779@users.noreply.github.com>
2022-06-28 16:29:05 -0400

    Create script.js

---
+++ Display input with Javascript/script.js
@@ -0,0 +1,8 @@
+/* I used button as selector just because there is only one button,
+make sure you are more precise with your selector */
+
+button.addEventListener("click", myFunc);
+function myFunc(){
+    var desiredEmail = document.querySelector('#email').value;
+    var letUserKnow = document.querySelector('#submited').innerHTML=`${desiredEmail} is subscribed!`;
+}
