3e723703d080200e7b59f0115214cd79605a515f
ik now <101111988+iknow779@users.noreply.github.com>
2022-06-29 07:38:04 -0400

    Update script.js

--- Display input with Javascript/script.js
+++ Display input with Javascript/script.js
@@ -1,7 +1,7 @@
 /* I used button as selector just because there is only one button,
 make sure you are more precise with your selector */
 
-button.addEventListener("click", myFunc);
+document.querySelector("button").addEventListener("click", myFunc);
 function myFunc(){
     var desiredEmail = document.querySelector('#email').value;
     var letUserKnow = document.querySelector('#submited').innerHTML=`${desiredEmail} is subscribed!`;
